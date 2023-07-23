f3d56823d5831dcefe0195139c4257d259c7135a
Prashareet <prashareet@gmail.com>
2023-07-23 18:35:31 +0530

    modified alert message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert("Press ok to play!");
+alert("Press ok to play!, refresh the page to play again!");
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 
